428898a95aff7789c04ca5430df0ec8226f580fa
vasundhara2507 <108589770+vasundhara2507@users.noreply.github.com>
2022-07-04 09:38:14 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+int sum=*a+*b;
+int dif;
+if(*a>*b){
+    dif=*a-*b;
+}
+else if(*b>*a){
+    dif=*b-*a;
+}
+*a=sum;
+*b=dif;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
